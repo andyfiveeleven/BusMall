1f954a627021427e4006de6c93d3248d5e1d261a
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-23 09:03:46 -0700

    did a reset and styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var imagesArray = [];
 var displayedImages = [];
 var lastShown = [];
 var counter = 0;
+var list = document.getElementById('list');
 
 //object constructor
 function ImageOption(name, path){
@@ -20,7 +21,7 @@ var breakfast = new ImageOption('breakfast', 'img/breakfast.jpg');
 var bubblegum = new ImageOption('bubblegum', 'img/bubblegum.jpg');
 var chair = new ImageOption('chair', 'img/chair.jpg');
 var cthulhu = new ImageOption('cthulhu', 'img/cthulhu.jpg');
-var dogduck = new ImageOption('dog-duck', 'img/dog-duck.jpg');
+var dogduck = new ImageOption('dog-duck', 'img/dog-duck.jpeg');
 var dragon = new ImageOption('dragon', 'img/dragon.jpg');
 var pen = new ImageOption('pen', 'img/pen.jpg');
 var petsweep = new ImageOption('pet-sweep', 'img/pet-sweep.jpg');
@@ -39,20 +40,20 @@ function randomImages(max){
     displayedImages.push(imagesArray[Math.floor(Math.random() * max)]);
   }
   // console.log(displayedImages);
-  while(displayedImages[0] === displayedImages[1] || displayedImages[0] === displayedImages[2] || displayedImages[1] === displayedImages[2]){
-    displayedImages.splice(1, 2);
-    displayedImages.push(imagesArray[Math.floor(Math.random() * max +1)]);
-    displayedImages.push(imagesArray[Math.floor(Math.random() * max +1)]);
-  }
+  // while(displayedImages[0] === displayedImages[1] || displayedImages[0] === displayedImages[2] || displayedImages[1] === displayedImages[2]){
+  //   displayedImages.splice(1, 2);
+  //   displayedImages.push(imagesArray[Math.floor(Math.random() * max +1)]);
+  //   displayedImages.push(imagesArray[Math.floor(Math.random() * max +1)]);
+  // }
   //compares current array[i] to last shown array, and to the other two positions in the current array, replaces if there is a similarity
   while(displayedImages[0] === lastShown[0] || displayedImages[0] === lastShown[1] || displayedImages[0] === lastShown[2] || displayedImages[0] === displayedImages[1] || displayedImages[0] === displayedImages[2]){
-    displayedImages.splice(0, 1, imagesArray[Math.floor(Math.random() * max +1)]);
+    displayedImages.splice(0, 1, imagesArray[Math.floor(Math.random() * max)]);
   }
   while(displayedImages[1] === lastShown[0] || displayedImages[1] === lastShown[1] || displayedImages[1] === lastShown[2] || displayedImages[1] === displayedImages[0] || displayedImages[1] === displayedImages[2]){
-    displayedImages.splice(1, 1, imagesArray[Math.floor(Math.random() * max +1)]);
+    displayedImages.splice(1, 1, imagesArray[Math.floor(Math.random() * max)]);
   }
   while(displayedImages[2] === lastShown[0] || displayedImages[2] === lastShown[1] || displayedImages[2] === lastShown[2] || displayedImages[2] === displayedImages[1] || displayedImages[2] === displayedImages[0]){
-    displayedImages.splice(2, 1, imagesArray[Math.floor(Math.random() * max +1)]);
+    displayedImages.splice(2, 1, imagesArray[Math.floor(Math.random() * max)]);
   }
   //end last and current display
   return displayedImages;
@@ -60,6 +61,15 @@ function randomImages(max){
 
 randomImages(imagesArray.length);
 
+
+function clicker(click) {
+  for (var i = 0; i < imagesArray.length; i++) {
+    if (imagesArray[i].name === click) {
+      imagesArray[i].clickCount ++;
+      console.log(imagesArray[i].clickCount);
+    }
+  }
+}
 
 function render(){
   for (var j = 0; j<3; j++) {
@@ -72,18 +82,32 @@ function render(){
     image.addEventListener('click', eventHandler);
     display.appendChild(image);
     object.displayCount++;
+    console.log(displayedImages);
   }
 }
 
 function eventHandler() {
-  if (counter < 25) {
-    this.clickCount ++;
+  if (counter < 24) {
+    var selected = event.target;
+    console.log(selected);
+    console.log(selected.id);
+    clicker(selected.id);
     counter ++;
     lastShown = displayedImages;
     displayedImages = [];
     document.getElementById('display').innerHTML = '';
     randomImages(imagesArray.length);
     render();
+    console.log(selected.clickCount);
+  } else {
+    document.getElementById('display').innerHTML = '';
+    var ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    for (var x = 0; x < imagesArray.length; x++){
+      var li = document.createElement('li');
+      li.innerHTML = imagesArray[x].name + ' was clicked ' + imagesArray[x].clickCount + ' times and was clicked ' + Math.floor(imagesArray[x].clickCount/imagesArray[x].displayCount * 100) + ' percent of times displayed';
+      list.appendChild(li);
+    }
   }
 }
 
---
+++ readme.md
@@ -0,0 +1,1 @@
+Colaborators: Nick, My Main Man. Eddie, Punk Rock God among Men. Clair, always helpful, especially when it comes to stupid questions. 
